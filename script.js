fb82e1473af5b061f525c31efd68bd8296c6c1e8
sharmababita <sharma.089babita@gmail.com>
2025-06-19 10:57:59 +0200

    sett inn verdier i box email osv

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
-console.log('Hello!');
+// Hent "knappen" som skal aktivere noe
+const hamburgerIcon = document.querySelector("#hamburgerIcon");
+console.log(hamburgerIcon);
+
+hamburgerIcon.addEventListener("click", function () {
+  // Hente elementet som skal bli synlig
+  const navList = document.querySelector("#navList");
+  console.log(navList);
+
+  navList.classList.remove("hidden");
+  navList.classList.add("navList");
+});
+
+const sunIcon = document.querySelector("#sunIcon");
+console.log(sunIcon);
+
+sunIcon.addEventListener("click", function () {
+  const body = document.querySelector("body");
+
+  body.classList.remove("light");
+  body.classList.add("dark");
+});
+
+
+//
+//
+//
+
+const loginButton = document.querySelector("#loginButton");
+console.log(loginButton);
+
+loginButton.addEventListener("click", function (event) {
+    event.preventDefault();
+
+console.log("Button clicked!");
+
+// Hent
+const userEmail = document.querySelector("#userEmail");
+const userPassword = document.querySelector("#userPassword");
+const viewPassword = document.querySelector("#viewPassword");
+const rememberLogin = document.querySelector("#rememberLogin");
+console.log(userEmail, userPassword, rememberLogin);
+
+// Modifiser  
+const emailValue = userEmail.value;
+const passwordValue = userPassword.value;
+
+// Send 
+console.log("Din epost addresse er: " + emailValue + ".")
+
+});
+
